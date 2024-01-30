539e4d41d5e26a204864dedae26d754126e06136
Miguel Sablan <miguelsablan1@outlook.com>
2024-01-30 12:45:28 -0500

    api set up, user signup

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,66 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, username, password } = body;
+
+    // Check if email already exists
+    const existingUserByEmail = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this email already exists",
+        },
+        { status: 409 }
+      );
+    }
+
+    // Check if username already exists
+    const existingUserByUsername = await prisma.user.findUnique({
+      where: { username: username },
+    });
+
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this username already exists",
+        },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    const { password: newUserPassword, ...rest } = newUser;
+
+    return NextResponse.json(
+      {
+        user: rest,
+        message: "User created successfully",
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Something went wrong...",
+      },
+      { status: 500 }
+    );
+  }
+}
