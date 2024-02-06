7cd9efae452d1064697f41d29bef49a0f9178e02
Miguel Sablan <miguelsablan1@outlook.com>
2024-02-06 17:44:17 -0500

    updated sign up page design

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,8 @@
-import Navbar from "@/components/Navbar";
 import SignUpForm from "@/components/SignUpForm";
 
 const SignUp = () => {
   return (
     <div className="w-full">
-      <Navbar />
       <SignUpForm />
     </div>
   );
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
 const registerSchema = z.object({
   username: z.string().min(1, "Username is required").max(100),
@@ -17,6 +21,8 @@ type RegisterSchema = z.infer<typeof registerSchema>;
 
 const SignUpForm = () => {
   const router = useRouter();
+
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
     register,
@@ -48,12 +54,17 @@ const SignUpForm = () => {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="bg-slate-200 p-10 rounded-md shadow-lg">
-        <h1 className="text-2xl text-gray-700 font-bold mb-4 text-center">
-          Sign Up
-        </h1>
+    <div className="grid place-items-center h-screen bg-gradient-to-tr from-orange-500 to-pink-500">
+      <div className="bg-white px-5 md:px-10 py-20 shadow-lg rounded-3xl md:w-[450px]">
+        <h1 className="text-5xl text-gray-700 font-semibold mb-4">Sign Up</h1>
+        <p className="font-medium text-md text-gray-500 my-4">
+          Welcome! Create an account to get started.
+        </p>
 
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
@@ -61,14 +72,14 @@ const SignUpForm = () => {
               Username
             </label>
             <input
-              className="border rounded w-full py-2 px-3"
+              className="border-2 rounded w-full py-2 px-3 mt-1"
               type="text"
               id="username"
-              placeholder="johndoe"
+              placeholder="Username"
               {...register("username", { required: true })}
             />
             {errors.username && (
-              <p className="text-md text-red-500 mt-1">
+              <p className="text-sm text-red-500 mt-1">
                 {errors.username?.message}
               </p>
             )}
@@ -79,39 +90,46 @@ const SignUpForm = () => {
               Email
             </label>
             <input
-              className="border rounded w-full py-2 px-3"
+              className="border-2 rounded w-full py-2 px-3 mt-1"
               type="email"
               id="email"
-              placeholder="mail@example.com"
+              placeholder="Email"
               {...register("email", { required: true })}
             />
             {errors.email && (
-              <p className="text-md text-red-500 mt-1">
+              <p className="text-sm text-red-500 mt-1">
                 {errors.email?.message}
               </p>
             )}
           </div>
 
-          <div className="mb-8">
-            <label className=" text-gray-700 text-md font-medium mb-2">
+          <div className="mb-8 relative">
+            <label className="text-gray-700 text-md font-medium mb-2">
               Password
             </label>
-            <input
-              className="border rounded w-full py-2 px-3"
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              {...register("password", { required: true })}
-            />
+            <div className="relative mt-1">
+              <input
+                className="border-2 rounded w-full py-2 px-3 pr-10"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="Password"
+                {...register("password", { required: true })}
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                className="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer text-gray-500"
+                onClick={togglePasswordVisibility}
+              />
+            </div>
             {errors.password && (
-              <p className="text-md text-red-500 mt-1">
+              <p className="text-sm text-red-500 mt-1">
                 {errors.password?.message}
               </p>
             )}
           </div>
 
           <button
-            className="w-full bg-black text-white py-2 px-4 rounded hover:opacity-75"
+            className="w-full bg-black text-white text-lg font-semibold py-2 px-4 rounded-lg uppercase hover:opacity-75 active:scale-95 active:duration-75 transition-all shadow-lg"
             type="submit"
           >
             Sign Up
@@ -122,15 +140,16 @@ const SignUpForm = () => {
           </div>
 
           <button
-            className="w-full bg-black text-white py-2 px-4 rounded hover:opacity-75"
+            className="w-full border py-3 px-6 rounded-full flex justify-center items-center gap-3 active:scale-95 active:duration-75 transition-all hover:opacity-75"
             onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
           >
+            <Image src="/google.svg" width={24} height={24} alt="Google Logo" />
             Sign up with Google
           </button>
 
-          <p className="text-center text-sm text-gray-600 mt-4">
+          <p className="text-center text-sm font-medium text-gray-600 mt-8">
             Already have an account?&nbsp;
-            <Link className="text-blue-500 hover:underline" href="/login">
+            <Link className="text-orange-500 hover:underline" href="/login">
               Login here
             </Link>
           </p>
