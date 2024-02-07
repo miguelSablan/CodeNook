1fb65dd1fc474c6df409dc2936d1328ad50c6940
MasterShakezula <90061079+MasterShakezula@users.noreply.github.com>
2024-02-06 22:02:13 -0500

    Create page.tsx
    Placeholder customization options screen for the user.

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,15 @@
+import LoginForm from "@/components/LoginForm";
+import Navbar from "@/components/Navbar";
+
+
+const Login = () => {
+  return (
+    <div className="w-full">
+      <h1 className = "bg-indigo-600">
+        Welcome to your customization screen.
+      </h1>
+    </div>
+  );
+};
+
+export default Login;
