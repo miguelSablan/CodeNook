2c9d8010377a3c94fe57ed85d7be549e3a251281
Miguel Sablan <miguelsablan1@outlook.com>
2024-03-24 23:20:23 -0400

    updated navbar buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,23 @@ const Navbar = async () => {
   const session = await getServerSession(authOptions);
 
   return (
-    <div className="flex items-center justify-between md:justify-around bg-gray-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
-      <Link className="ml-4" href="/">
-        Home
+    <div className="flex items-center justify-between h-14 md:justify-around bg-gray-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+      <Link className="text-2xl font-bold ml-4" href="/">
+        Code<span className="text-blue-700">Nook</span>
       </Link>
 
-      <Link
-        className="mr-4 bg-black text-white py-2 px-4 rounded hover:opacity-75"
-        href="/login"
-      >
-        Login
-      </Link>
+      <div className="mr-4">
+        <Link className="py-2 mr-3 rounded hover:opacity-75" href="/login">
+          Log in
+        </Link>
+
+        <Link
+          className="rounded-full border border-black bg-black px-4 py-1.5 text-sm text-white transition-all hover:opacity-75"
+          href="/signup"
+        >
+          Sign Up
+        </Link>
+      </div>
     </div>
   );
 };
