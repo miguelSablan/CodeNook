27febf2286ba304ecc8784485a2b31b89bcdcfec
MasterShakezula <90061079+MasterShakezula@users.noreply.github.com>
2024-04-01 00:06:29 -0400

    Logo (placehoplder) + image provider
    Added remote patterns array to next.config.js to approve images fetched from freepik.com. Added picture to landing page

--- next.config.js
+++ next.config.js
@@ -2,3 +2,17 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports=
+{
+    images:
+    {
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'img.freepik.com',
+                port: '',
+            }
+        ]
+    }
+}
