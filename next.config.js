c41758fb7617dab02ee4f4f7a65dc6c6338ae22e
Miguel Sablan <miguelsablan1@outlook.com>
2024-05-10 17:05:28 -0400

    tweaked formatting, imported image

--- next.config.js
+++ next.config.js
@@ -9,12 +9,6 @@ const nextConfig = {
         port: "",
         pathname: "**",
       },
-      {
-        protocol: "https",
-        hostname: "img.freepik.com",
-        port: "",
-        pathname: "**",
-      },
     ],
   },
 };
