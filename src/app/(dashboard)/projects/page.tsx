c233fd7283ce7cd84b2427538a9d7aed38c5b6fa
Miguel Sablan <miguelsablan1@outlook.com>
2024-05-16 13:16:05 -0400

    redid card design

--- src/app/(dashboard)/projects/page.tsx
+++ src/app/(dashboard)/projects/page.tsx
@@ -4,39 +4,55 @@ const Dashboard = () => {
   const projects = [
     {
       id: 1,
-      imgUrl:
-        "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg",
+      date: "Jan 14, 2024",
       title: "E-commerce Website",
       description:
-        "Build an online store where users can browse products, add them to cart, and checkout. Use React for frontend components and Tailwind CSS for styling. Implement features like product search, filters, and a user authentication system.",
+        "We are building an innovative e-commerce platform revolutionizing online shopping. We're seeking passionate React developers with expertise in Tailwind CSS and TypeScript to join our frontend team. Help us create a seamless user experience with advanced product search, filters, and a robust authentication system.",
       tags: ["React", "Tailwind", "TypeScript"],
+      author: {
+        name: "Username",
+        avatarUrl:
+          "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
+      },
     },
     {
       id: 2,
-      imgUrl:
-        "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg",
+      date: "Jan 12, 2024",
       title: "Image Processing App",
       description:
-        "Create a tool for image processing and manipulation using Python and OpenCV. Include features like image filters, object detection, and image enhancement. Allow users to upload images, apply filters, and download the processed images.",
+        "Join us in developing a cutting-edge image processing app powered by Python and OpenCV. We're looking for skilled Python developers with experience in openCV to enhance our image manipulation features, including filters, object detection, and image enhancement. Collaborate with us to create a powerful tool for image professionals and enthusiasts.",
       tags: ["Python", "openCV"],
+      author: {
+        name: "Username 2",
+        avatarUrl:
+          "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
+      },
     },
     {
       id: 3,
-      imgUrl:
-        "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg",
+      date: "Jan 11, 2024",
       title: "Social Media Dashboard",
       description:
-        "Develop a dashboard application for managing social media accounts. Integrate APIs for platforms like Twitter, Facebook, and Instagram to fetch data. Display analytics, post scheduling, and engagement metrics for each social media account.",
-      tags: ["React"],
+        "Join our team as a UI/UX designer to develop a dashboard application for managing social media accounts. Integrate APIs for platforms like Twitter, Facebook, and Instagram to fetch data. Display analytics, post scheduling, and engagement metrics for each social media account.",
+      tags: ["UI/UX", "React"],
+      author: {
+        name: "Username 3",
+        avatarUrl:
+          "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
+      },
     },
     {
       id: 4,
-      imgUrl:
-        "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg",
+      date: "Jan 10, 2024",
       title: "3D Game",
       description:
-        "Design and develop a 3D game using Unity game engine. Create game mechanics, levels, and interactive elements. Include features like player progression, scoring system, and in-game rewards.",
+        "Join our team of game developers to create an immersive 3D gaming experience using Unity. We're seeking Unity experts to design game mechanics, levels, and interactive elements. Contribute to player progression, scoring systems, and in-game rewards to delight gamers worldwide.",
       tags: ["Unity"],
+      author: {
+        name: "Username 4",
+        avatarUrl:
+          "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
+      },
     },
   ];
 
@@ -46,18 +62,6 @@ const Dashboard = () => {
         <h1 className="text-white text-4xl p-4">Projects</h1>
 
         <div className="flex p-4 gap-3">
-          <div className="flex items-center">
-            <label className="text-white mr-2">Filter by:</label>
-            <select className="select select-bordered text-black">
-              <option>All Categories</option>
-              <option>Frontend</option>
-              <option>Backend</option>
-              <option>UI/UX</option>
-              <option>AI</option>
-              <option>VR</option>
-            </select>
-          </div>
-
           <label className="input input-bordered flex items-center gap-2 w-1/2">
             <input
               type="text"
@@ -86,26 +90,33 @@ const Dashboard = () => {
               {projects.map((project) => (
                 <div
                   key={project.id}
-                  className="card lg:card-side bg-base-100 shadow-xl text-black"
+                  className="flex flex-col gap-6 bg-[#1d1d1d] text-white rounded-box p-6 max-w-auto"
                 >
-                  <figure>
-                    <img src={project.imgUrl} alt={project.title} />
-                  </figure>
-                  <div className="card-body md:w-1/2">
-                    <h2 className="card-title">{project.title}</h2>
-                    <p>{project.description}</p>
-                    <div className="flex justify-start flex-wrap gap-2">
+                  <div className="flex justify-between items-center">
+                    <span className="text-sm">{project.date}</span>
+                    <div className="flex gap-3">
                       {project.tags.map((tag, index) => (
-                        <p
-                          key={index}
-                          className="badge badge-primary max-w-max"
-                        >
+                        <div key={index} className="btn btn-sm btn-primary">
                           {tag}
-                        </p>
+                        </div>
                       ))}
                     </div>
-                    <div className="card-actions justify-end">
-                      <button className="btn btn-primary">Interested</button>
+                  </div>
+
+                  <div className="flex flex-col gap-2">
+                    <h1 className="text-2xl font-bold">{project.title}</h1>
+                    <span>{project.description}</span>
+                  </div>
+
+                  <div className="flex justify-between items-center">
+                    <button className="btn btn-primary">Apply</button>
+                    <div className="btn btn-ghost">
+                      <img
+                        src={project.author.avatarUrl}
+                        alt="Avatar"
+                        className="w-8 rounded-full"
+                      />
+                      {project.author.name}
                     </div>
                   </div>
                 </div>
@@ -115,7 +126,17 @@ const Dashboard = () => {
 
           <div className="col-span-1">
             <div className="p-4 h-full">
-              <h1 className="text-white">Placeholder</h1>
+              <div className="flex items-center">
+                <label className="text-white mr-2">Filter by:</label>
+                <select className="select select-bordered text-black">
+                  <option>All Categories</option>
+                  <option>Frontend</option>
+                  <option>Backend</option>
+                  <option>UI/UX</option>
+                  <option>AI</option>
+                  <option>VR</option>
+                </select>
+              </div>
             </div>
           </div>
         </div>
