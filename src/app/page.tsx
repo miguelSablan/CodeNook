f27d296e895ab30ee1d606f7f80843c504b8c373
Miguel Sablan <miguelsablan1@outlook.com>
2024-05-21 21:19:18 -0400

    landing page design tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,25 @@
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
-import Tree from "/public/tree.png";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-center p-24 overflow-auto">
+    <main className="flex flex-col min-h-screen items-center justify-center md:p-24 overflow-auto bg-[#1d1d1d] text-white">
       <Navbar />
-      <div className="w-full max-w-screen-xl px-2.5 md:px-20 pb-28 pt-28 flex flex-col items-center justify-center text-center sm:pt-40 ">
+      <div className="w-full max-w-screen-xl px-2.5 md:px-20 pb-28 pt-28 flex flex-col items-center justify-center text-center sm:pt-40">
         <h1 className="font-bold text-5xl md:text-6xl lg:text-7xl">
-          Welcome to
-          <span className="text-blue-700"> CodeNook! </span>
           Connect, Collaborate, and Create
           <span className="text-blue-700"> Masterpieces</span>
         </h1>
 
-        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        <p className="mt-5 max-w-prose text-zinc-400 sm:text-lg">
           Empowering programmers to connect, collaborate, and unleash their
           creativity in a vibrant coding community.
         </p>
 
         <div className="mt-6 flex items-center gap-8">
           <Link
-            className="inline-flex items-center justify-center rounded-full text-sm font-medium border border-black bg-black px-8 h-11 text-white transition-all hover:opacity-75"
+            className="inline-flex items-center justify-center rounded-full text-sm font-medium border border-black bg-primary px-8 h-11 text-white transition-all hover:opacity-75"
             href="/signup"
           >
             Get started
@@ -47,12 +44,12 @@ export default function Home() {
         <div>
           <div className="mx-auto max-w-6xl px-6 lg:px-8">
             <div className="mt-16 flow-root sm:mt-24">
-              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <div className="-m-2 rounded-xl bg-gray-100/5 p-2 ring-1 ring-inset ring-white lg:-m-4 lg:rounded-2xl lg:p-4">
                 <Image
-                  src={Tree}
-                  alt="Tree"
-                  width={500}
-                  height={500}
+                  src={"/preview.png"}
+                  alt="Website Preview"
+                  width={1364}
+                  height={866}
                   quality={100}
                   className="rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10"
                 />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,37 @@
-import Link from "next/link";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+"use client";
 
-const Navbar = async () => {
-  const session = await getServerSession(authOptions);
+import Link from "next/link";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+
+const Navbar = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setHasScrolled(true);
+      } else {
+        setHasScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className="flex items-center justify-between h-14 md:justify-around bg-gray-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
-      <Link className="text-2xl font-bold ml-4" href="/">
-        Code<span className="text-blue-700">Nook</span>
+    <div
+      className={`flex items-center justify-between h-14 md:justify-around bg-[#1d1d1d] py-2 fixed w-full z-10 top-0 transition-all ${
+        hasScrolled ? "border-b border-s-zinc-200" : ""
+      }`}
+    >
+      <Link className="btn btn-ghost text-2xl font-bold" href="/">
+        <FontAwesomeIcon icon={faCodeBranch} />
+        <div>
+          <span className="text-blue-600">Code</span>Nook
+        </div>
       </Link>
 
       <div className="mr-4">
@@ -17,7 +40,7 @@ const Navbar = async () => {
         </Link>
 
         <Link
-          className="rounded-full border border-black bg-black px-4 py-1.5 text-sm text-white transition-all hover:opacity-75"
+          className="rounded-full border border-black bg-primary px-4 py-1.5 text-sm text-white transition-all hover:opacity-75"
           href="/signup"
         >
           Sign Up
