f94efa6df2ca5b152c5a163be8e7820ebdee5b7e
MIGUEL <msabl005@fiu.edu>
2024-06-17 16:25:37 -0400

    tweaked footer position and brand images

--- src/components/FooterTest.tsx
+++ src/components/FooterTest.tsx
@@ -1,45 +1,59 @@
-import React from 'react';
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebook,
+  faGithub,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 const FooterTest = () => {
-    return (
-        <footer className=" flex flex-col sm:flex-row gap-64 justify-between p-10 bg-base-200">    
-            <aside>
-                <div>
-                    <Link className="" href="/">
-                        <p className="text-3xl flex items-center gap-0 font-bold">
-                            
-                            <div className="inline w-9">
-                                    <FontAwesomeIcon icon = {faCodeBranch}/>                   
-                            </div>                    
-                            <span className ="text-blue-600 ">Code</span>Nook
-                        </p>
-                    </Link>
-                    <small>Copyright © 2024 - All rights reserved</small>
-                </div>
-            </aside>
-            
-            <div>
-                <nav className="flex gap-0">
-                    <a className="btn btn-ghost btn-md btn-circle">                
-                        <img src = "/github.svg"></img>
-                    </a>
-                    <a className="btn btn-ghost btn-md btn-circle">
-                        <img src = "/google.svg"></img>
-                    </a>
-                    <a className="btn btn-ghost btn-md btn-circle">
-                        <img src = "/discord.svg"></img>
-                    </a>
-                    <a className="btn btn-ghost btn-md btn-circle">
-                        <img src = "/next.svg"></img>
-                    </a>
-                </nav>
-            </div>    
-        </footer>
-        
-    );
+  return (
+    <footer className="flex flex-col sm:flex-row gap-8 justify-between p-10 bg-black w-full">
+      <aside>
+        <Link className="text-3xl flex items-center gap-2 font-bold" href="/">
+          <FontAwesomeIcon icon={faCodeBranch} />
+          <div>
+            <span className="text-blue-600">Code</span>Nook
+          </div>
+        </Link>
+        <small>Copyright © 2024 - All rights reserved</small>
+      </aside>
+
+      <nav className="flex gap-0">
+        <Link
+          className="btn btn-ghost btn-md btn-circle text-2xl"
+          href="https://github.com/miguelSablan/CodeNook"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faGithub as IconProp} />
+        </Link>
+        <Link
+          className="btn btn-ghost btn-md btn-circle text-2xl"
+          href="/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faTwitter as IconProp} />
+        </Link>
+        <Link
+          className="btn btn-ghost btn-md btn-circle text-2xl"
+          href="/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faFacebook as IconProp} />
+        </Link>
+        <Link
+          className="btn btn-ghost btn-md btn-circle text-2xl"
+          href="/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faYoutube as IconProp} />
+        </Link>
+      </nav>
+    </footer>
+  );
 };
 
 export default FooterTest;
