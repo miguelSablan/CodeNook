a34df4c48ef3f69f37dff75f7c49c460d9315f6f
MasterShakezula <90061079+MasterShakezula@users.noreply.github.com>
2024-06-17 01:10:07 -0400

    Last Update for today
    Halfway done with CodeNook footer. Includes logos for github, youtube, discord.

--- src/components/FooterTest.tsx
+++ src/components/FooterTest.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 const App = () => {
     return (
         <footer className="flex flex-col sm:flex-row gap-8 justify-between p-10 bg-base-200">    
             <aside>
-                <p className="text-3xl flex items-center gap-0 ">
-                    <div className="inline w-9">
-                            <FontAwesomeIcon icon = {faCodeBranch}/>                   
-                    </div>                    
-                    <span className ="text-blue-600">Code</span>Nook
-                </p>
-                <small>Copyright © 2024 - All rights reserved</small>
+                <div>
+                    <Link className="" href="/">
+                        <p className="text-3xl flex items-center gap-0 font-bold">
+                            
+                            <div className="inline w-9">
+                                    <FontAwesomeIcon icon = {faCodeBranch}/>                   
+                            </div>                    
+                            <span className ="text-blue-600 ">Code</span>Nook
+                        </p>
+                    </Link>
+                    <small>Copyright © 2024 - All rights reserved</small>
+                </div>
             </aside>
    
             <nav className="flex gap-4">
-                <a className="btn btn-ghost btn-sm btn-circle">
-                    <i className="fa-brands fa-github text-2xl"></i>
+                <a className="btn btn-ghost btn-sm btn-circle">                
+                    <img src = "/github.svg"></img>
                 </a>
                 <a className="btn btn-ghost btn-sm btn-circle">
-                    <i className="fa-brands fa-twitter text-2xl"></i>
+                    <img src = "/google.svg"></img>
                 </a>
                 <a className="btn btn-ghost btn-sm btn-circle">
-                    <i className="fa-brands fa-facebook text-2xl"></i>
+                    <img src = "/discord.svg"></img>
                 </a>
                 <a className="btn btn-ghost btn-sm btn-circle">
-                    <i className="fa-brands fa-youtube text-2xl"></i>
+                    <img src = "/next.svg"></img>
                 </a>
             </nav>
 </footer>
