117d124b3ae130283ec6b3ee53a43e687051885f
Miguel Sablan <miguelsablan1@outlook.com>
2024-07-01 01:13:16 -0400

    replace usesession with profile endpoint

---
+++ src/app/api/profile/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import prisma from "@/lib/prisma";
+import { authOptions } from "@/lib/auth";
+
+export async function GET(req: Request) {
+  try {
+    // Authenticate the user
+    const session = await getServerSession({ req, ...authOptions });
+
+    if (!session) {
+      return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+    }
+
+    // Get the user ID from the session
+    const userId = session.user.id;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        email: true,
+        image: true,
+        bio: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
