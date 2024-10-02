6f6a9b4e34e3bc5287074e0f0c433f4bfdb63c23
Miguel Sablan <miguelsablan1@outlook.com>
2024-10-02 01:36:07 -0400

    added link for applicants in list

--- src/components/ViewApplicantsModal.tsx
+++ src/components/ViewApplicantsModal.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface Applicant {
   id: number;
@@ -32,30 +33,35 @@ function ViewApplicantsModal({ applicants }: ViewApplicantsModalProps) {
                   key={applicant.id}
                   className="flex items-center p-3 bg-gray-800 rounded-lg"
                 >
-                  <div className="w-12 h-12 rounded-full mr-4">
-                    {applicant.user.image ? (
-                      <Image
-                        src={applicant.user.image}
-                        className="rounded-full"
-                        alt={`${applicant.user.name}'s avatar`}
-                        height="128"
-                        width="128"
-                        priority
-                        layout="intrinsic"
-                      />
-                    ) : (
-                      <div className="rounded-full bg-blue-500 h-full w-full text-white text-lg leading-[128px] flex items-center justify-center">
-                        <span className="text-white">
-                          {applicant.user.name?.charAt(0).toUpperCase()}
-                        </span>
-                      </div>
-                    )}
-                  </div>
-                  <div className="flex flex-col flex-1">
-                    <p className="text-white text-lg font-semibold">
-                      {applicant.user.name}
-                    </p>
-                  </div>
+                  <Link
+                    href={`/dashboard/users/${applicant.user.id}`}
+                    className="flex items-center flex-1"
+                  >
+                    <div className="w-12 h-12 rounded-full mr-4">
+                      {applicant.user.image ? (
+                        <Image
+                          src={applicant.user.image}
+                          className="rounded-full"
+                          alt={`${applicant.user.name}'s avatar`}
+                          height="128"
+                          width="128"
+                          priority
+                          layout="intrinsic"
+                        />
+                      ) : (
+                        <div className="rounded-full bg-blue-500 h-full w-full text-white text-lg leading-[128px] flex items-center justify-center">
+                          <span className="text-white">
+                            {applicant.user.name?.charAt(0).toUpperCase()}
+                          </span>
+                        </div>
+                      )}
+                    </div>
+                    <div className="flex flex-col">
+                      <p className="text-white text-lg font-semibold">
+                        {applicant.user.name}
+                      </p>
+                    </div>
+                  </Link>
                   <button className="btn btn-neutral ml-4">Accept</button>
                 </li>
               ))}
