b29c6686ae9ab5f74b4a6293068b8652a62058c6
Miguel Sablan <miguelsablan1@outlook.com>
2024-10-01 23:28:27 -0400

    fixed image for users with no avatar

--- src/components/ViewApplicantsModal.tsx
+++ src/components/ViewApplicantsModal.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface Applicant {
   id: number;
   user: {
@@ -30,11 +32,25 @@ function ViewApplicantsModal({ applicants }: ViewApplicantsModalProps) {
                   key={applicant.id}
                   className="flex items-center p-3 bg-gray-800 rounded-lg"
                 >
-                  <img
-                    src={applicant.user.image}
-                    alt={`${applicant.user.name}'s avatar`}
-                    className="w-12 h-12 rounded-full mr-4"
-                  />
+                  <div className="w-12 h-12 rounded-full mr-4">
+                    {applicant.user.image ? (
+                      <Image
+                        src={applicant.user.image}
+                        className="rounded-full"
+                        alt={`${applicant.user.name}'s avatar`}
+                        height="128"
+                        width="128"
+                        priority
+                        layout="intrinsic"
+                      />
+                    ) : (
+                      <div className="rounded-full bg-blue-500 h-full w-full text-white text-lg leading-[128px] flex items-center justify-center">
+                        <span className="text-white">
+                          {applicant.user.name?.charAt(0).toUpperCase()}
+                        </span>
+                      </div>
+                    )}
+                  </div>
                   <div className="flex flex-col flex-1">
                     <p className="text-white text-lg font-semibold">
                       {applicant.user.name}
