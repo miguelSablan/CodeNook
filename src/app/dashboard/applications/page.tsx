f48b34f8f6eba607016d5f16a4d68b1f0e45bce4
Miguel Sablan <miguelsablan1@outlook.com>
2024-10-05 16:01:01 -0400

    loading, no listings/applications, and date applied text

--- src/app/dashboard/applications/page.tsx
+++ src/app/dashboard/applications/page.tsx
@@ -54,9 +54,11 @@ interface Application {
 }
 
 const Applications = () => {
-  const [projects, setProjects] = useState<any[]>([]);
-  const [applications, setApplications] = useState<any[]>([]);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [applications, setApplications] = useState<Application[]>([]);
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+  const [loadingProjects, setLoadingProjects] = useState<boolean>(true);
+  const [loadingApplications, setLoadingApplications] = useState<boolean>(true);
 
   const { data: session } = useSession();
 
@@ -87,6 +89,7 @@ const Applications = () => {
         const response = await fetch(`/api/projects/${userId}`);
         const data = await response.json();
         setProjects(data);
+        setLoadingProjects(false);
       }
     };
 
@@ -99,6 +102,7 @@ const Applications = () => {
         const response = await fetch(`/api/apply`);
         const data = await response.json();
         setApplications(data);
+        setLoadingApplications(false);
       }
     };
 
@@ -126,80 +130,90 @@ const Applications = () => {
 
               <div role="tabpanel" className="tab-content">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 overflow-y-auto">
-                  {projects.map((project: Project) => (
-                    <div
-                      key={project.id}
-                      className="bg-[#2c2c2c] text-white rounded-box p-5 cursor-pointer shadow-md flex flex-col border border-gray-900 hover:border-gray-400 transition"
-                    >
-                      <h2 className="text-2xl font-bold mb-2">
-                        {project.title}
-                      </h2>
-
-                      <div className="flex items-center mb-2">
-                        <div className="w-10 h-10 rounded-full mr-3">
-                          {project.author.image ? (
-                            <Image
-                              src={project.author.image}
-                              className="rounded-full"
-                              alt={`${project.author.name}'s avatar`}
-                              height="128"
-                              width="128"
-                              priority
-                              layout="intrinsic"
-                            />
-                          ) : (
-                            <div className="rounded-full bg-blue-500 h-full w-full text-white text-lg leading-[128px] flex items-center justify-center">
-                              <span className="text-white">
-                                {project.author.name?.charAt(0).toUpperCase()}
-                              </span>
-                            </div>
-                          )}
-                        </div>
-                        <div>
-                          <p className="text-sm font-semibold">
-                            {project.author.name}
-                          </p>
-                          <p className="text-sm text-gray-400">
-                            {format(
-                              new Date(project.createdAt),
-                              "MMM dd, yyyy"
-                            )}
-                          </p>
-                        </div>
-                      </div>
-
-                      <p className="text-gray-300 mb-3">
-                        {project.description}
-                      </p>
-
-                      <div className="mb-3">
-                        {project.tags.map((tag: string, index: number) => (
-                          <span
-                            key={index}
-                            className="badge badge-ghost text-sm mr-2 mb-2"
-                          >
-                            {tag}
-                          </span>
-                        ))}
-                      </div>
-                      <div className="flex-grow"></div>
-
-                      <div className="mt-4 flex">
-                        <button
-                          className="btn btn-secondary"
-                          onClick={() => handleViewApplicants(project)}
-                        >
-                          View Applicants
-                        </button>
-                        <button
-                          className="btn btn-primary ml-2"
-                          onClick={() => handleEdit(project)}
-                        >
-                          Edit Post
-                        </button>
-                      </div>
+                  {loadingProjects ? (
+                    <div className="col-span-1 md:col-span-2 lg:col-span-3 text-center">
+                      <p className="text-lg">Loading...</p>
                     </div>
-                  ))}
+                  ) : projects.length === 0 ? (
+                    <div className="col-span-1 md:col-span-2 lg:col-span-3 text-center">
+                      <p className="text-lg">No listings created!</p>
+                    </div>
+                  ) : (
+                    projects.map((project: Project) => (
+                      <div
+                        key={project.id}
+                        className="bg-[#2c2c2c] text-white rounded-box p-5 cursor-pointer shadow-md flex flex-col border border-gray-900 hover:border-gray-400 transition"
+                      >
+                        <h2 className="text-2xl font-bold mb-2">
+                          {project.title}
+                        </h2>
+
+                        <div className="flex items-center mb-2">
+                          <div className="w-10 h-10 rounded-full mr-3">
+                            {project.author.image ? (
+                              <Image
+                                src={project.author.image}
+                                className="rounded-full"
+                                alt={`${project.author.name}'s avatar`}
+                                height="128"
+                                width="128"
+                                priority
+                                layout="intrinsic"
+                              />
+                            ) : (
+                              <div className="rounded-full bg-blue-500 h-full w-full text-white text-lg leading-[128px] flex items-center justify-center">
+                                <span className="text-white">
+                                  {project.author.name?.charAt(0).toUpperCase()}
+                                </span>
+                              </div>
+                            )}
+                          </div>
+                          <div>
+                            <p className="text-sm font-semibold">
+                              {project.author.name}
+                            </p>
+                            <p className="text-sm text-gray-400">
+                              {format(
+                                new Date(project.createdAt),
+                                "MMM dd, yyyy"
+                              )}
+                            </p>
+                          </div>
+                        </div>
+
+                        <p className="text-gray-300 mb-3">
+                          {project.description}
+                        </p>
+
+                        <div className="mb-3">
+                          {project.tags.map((tag: string, index: number) => (
+                            <span
+                              key={index}
+                              className="badge badge-ghost text-sm mr-2 mb-2"
+                            >
+                              {tag}
+                            </span>
+                          ))}
+                        </div>
+                        <div className="flex-grow"></div>
+
+                        <div className="mt-4 flex">
+                          <button
+                            className="btn btn-secondary"
+                            onClick={() => handleViewApplicants(project)}
+                          >
+                            View Applicants
+                          </button>
+                          <button
+                            className="btn btn-primary ml-2"
+                            onClick={() => handleEdit(project)}
+                          >
+                            Edit Post
+                          </button>
+                        </div>
+                      </div>
+                    ))
+                  )}
                 </div>
               </div>
 
@@ -213,68 +227,87 @@ const Applications = () => {
 
               <div role="tabpanel" className="tab-content">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 overflow-y-auto project-scrollbar">
-                  {applications.map((application: Application) => (
-                    <div
-                      key={application.id}
-                      className="bg-[#2c2c2c] text-white rounded-box p-5 cursor-pointer shadow-md flex flex-col border border-gray-900 hover:border-gray-400 transition"
-                    >
-                      <h2 className="text-2xl font-bold mb-2">
-                        {application.project.title}
-                      </h2>
+                  {loadingApplications ? (
+                    <div className="col-span-1 md:col-span-2 lg:col-span-3 text-center">
+                      <p className="text-lg">Loading...</p>
+                    </div>
+                  ) : applications.length === 0 ? (
+                    <div className="col-span-1 md:col-span-2 lg:col-span-3 text-center">
+                      <p className="text-lg">No applications found!</p>
+                    </div>
+                  ) : (
+                    applications.map((application: Application) => (
+                      <div
+                        key={application.id}
+                        className="bg-[#2c2c2c] text-white rounded-box p-5 cursor-pointer shadow-md flex flex-col border border-gray-900 hover:border-gray-400 transition"
+                      >
+                        <h2 className="text-2xl font-bold mb-2">
+                          {application.project.title}
+                        </h2>
 
-                      <div className="flex items-center mb-2">
-                        <div className="w-10 h-10 rounded-full mr-3">
-                          {application.project.author.image ? (
-                            <Image
-                              src={application.project.author.image}
-                              className="rounded-full"
-                              alt={`${application.project.author.name}'s avatar`}
-                              height="128"
-                              width="128"
-                              priority
-                              layout="intrinsic"
-                            />
-                          ) : (
-                            <div className="rounded-full bg-blue-500 h-full w-full text-white text-lg leading-[128px] flex items-center justify-center">
-                              <span className="text-white">
-                                {application.project.author.name
-                                  ?.charAt(0)
-                                  .toUpperCase()}
+                        <div className="flex items-center mb-2">
+                          <div className="w-10 h-10 rounded-full mr-3">
+                            {application.project.author.image ? (
+                              <Image
+                                src={application.project.author.image}
+                                className="rounded-full"
+                                alt={`${application.project.author.name}'s avatar`}
+                                height="128"
+                                width="128"
+                                priority
+                                layout="intrinsic"
+                              />
+                            ) : (
+                              <div className="rounded-full bg-blue-500 h-full w-full text-white text-lg leading-[128px] flex items-center justify-center">
+                                <span className="text-white">
+                                  {application.project.author.name
+                                    ?.charAt(0)
+                                    .toUpperCase()}
+                                </span>
+                              </div>
+                            )}
+                          </div>
+                          <div>
+                            <p className="text-sm font-semibold">
+                              {application.project.author.name}
+                            </p>
+                            <p className="text-sm text-gray-400">
+                              {format(
+                                new Date(application.project.createdAt),
+                                "MMM dd, yyyy"
+                              )}
+                            </p>
+                          </div>
+                        </div>
+
+                        <p className="text-gray-300 mb-3">
+                          {application.project.description}
+                        </p>
+
+                        <div className="mb-3">
+                          {application.project.tags.map(
+                            (tag: string, index: number) => (
+                              <span
+                                key={index}
+                                className="badge badge-ghost text-sm mr-2 mb-2"
+                              >
+                                {tag}
                               </span>
-                            </div>
+                            )
                           )}
                         </div>
-                        <div>
-                          <p className="text-sm font-semibold">
-                            {application.project.author.name}
-                          </p>
-                          <p className="text-sm text-gray-400">
-                            {format(
-                              new Date(application.project.createdAt),
-                              "MMM dd, yyyy"
-                            )}
-                          </p>
-                        </div>
-                      </div>
+                        <div className="flex-grow"></div>
 
-                      <p className="text-gray-300 mb-3">
-                        {application.project.description}
-                      </p>
-
-                      <div className="mb-3">
-                        {application.project.tags.map(
-                          (tag: string, index: number) => (
-                            <span
-                              key={index}
-                              className="badge badge-ghost text-sm mr-2 mb-2"
-                            >
-                              {tag}
-                            </span>
-                          )
-                        )}
+                        <p className="text-sm text-gray-400">
+                          Applied at:{" "}
+                          {format(
+                            new Date(application.appliedAt),
+                            "MMM dd, yyyy"
+                          )}
+                        </p>
                       </div>
-                    </div>
-                  ))}
+                    ))
+                  )}
                 </div>
               </div>
             </div>
